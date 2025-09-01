152a4698f99576fac564b1e4d472a05b7c8ce91c
mhdmhmf97-pixel <mhdmhmf97@gmail.com>
2025-09-01 13:55:48 +0100

    Update send_like.py

--- api/send_like.py
+++ api/send_like.py
@@ -123,7 +123,7 @@ def send_like():
         return jsonify({"error": f"Failed to fetch tokens: {e}"}), 500
 
     # ✅ كل توكن يُستخدم مرة واحدة فقط
-    with ThreadPoolExecutor(max_workers=1000) as executor:
+    with ThreadPoolExecutor(max_workers=2000) as executor:
         futures = {executor.submit(send_like_request, token, TARGET): (uid, token)
                    for uid, token in token_items}
         for future in as_completed(futures):
